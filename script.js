1e19904d0f2a590ae82cbea8916b23858156b5cd
akashnagar98 <133683608+akashnagar98@users.noreply.github.com>
2024-06-26 20:56:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function displayLoveLetter() {
+    const loveLetter = `
+        Dear [Your Crush's Name],
+
+        Roses are red,
+        Violets are blue,
+        Sugar is sweet,
+        And so are you.
+
+        From the moment I met you, I knew you were special.
+        Your smile lights up my world, and your laughter is music to my ears.
+
+        Will you be my [girlfriend/boyfriend/partner]?
+
+        With all my heart,
+        [Your Name]
+    `;
+    document.getElementById('love-letter').innerText = loveLetter;
+}
